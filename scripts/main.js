221c64036648e50352ef222c3cf4142b4bfe9ab4
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2023-04-29 20:17:06 +0600

    feat: add keyboard first row (Sat, Apr 29, 2023  8:17:06 PM)

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,54 @@
-import createKey from "./modules/keyboard-key";
+// eslint-disable-next-line import/extensions
+import createRow from "./modules/keyboard-row.js";
+
+const head = document.querySelector("head");
+const styleLink = document.createElement("link");
+styleLink.setAttribute("rel", "stylesheet");
+styleLink.setAttribute("href", "./style.css");
+head.appendChild(styleLink);
+
+const keyboardCharsRu = [
+  [
+    "ё",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "Backspace",
+  ],
+];
+const keyboardCharsEn = [
+  [
+    "`",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "Backspace",
+  ],
+];
 
 const bodyPage = document.querySelector("body");
 const keyboardWrapper = document.createElement("div");
-const keyboardRow = document.createElement("div");
-// const keyboardKey = document.createElement("div");
+const keyboardRow = createRow(keyboardCharsRu[0], keyboardCharsEn[0]);
 
-// const keyboardCharsRu = [
-//   [
-//     "ё",
-//     "1",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "0",
-//     "-",
-//     "=",
-//     "Backspace",
-//   ],
-// ];
-// const keyboardCharsEn = [
-//   [
-//     "`",
-//     "1",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "0",
-//     "-",
-//     "=",
-//     "Backspace",
-//   ],
-// ];
+keyboardWrapper.className = "body__keyboard keyboard";
 
-// keyboardWrapper.className = "body__keyboard keyboard";
-// keyboardRow.className = "keyboard__row row";
-// keyboardKey.className = "keyboard__key key";
-
-const keyboardKey = createKey("й", "q");
-keyboardRow.appendChild(keyboardKey);
 keyboardWrapper.appendChild(keyboardRow);
 bodyPage.appendChild(keyboardWrapper);
--- scripts/modules/keyboard-key.js
+++ scripts/modules/keyboard-key.js
@@ -6,14 +6,18 @@ export default function createKey(rus, eng) {
   const caseUp = document.createElement("span");
   const caps = document.createElement("span");
   const shiftCaps = document.createElement("span");
+  const caseDownEng = document.createElement("span");
+  const caseUpEng = document.createElement("span");
+  const capsEng = document.createElement("span");
+  const shiftCapsEng = document.createElement("span");
 
   key.className = `keyboard__key key key${eng.toUpperCase()}`;
   keyRus.className = "rus";
   keyEng.className = "eng";
   caseDown.className = "caseDown";
-  caseUp.className = "caseUp";
-  caps.className = "caps";
-  shiftCaps.className = "shiftCaps";
+  caseUp.className = "caseUp hidden";
+  caps.className = "caps hidden";
+  shiftCaps.className = "shiftCaps hidden";
 
   caseDown.innerHTML = rus;
   caseUp.innerHTML = rus.toUpperCase();
@@ -25,15 +29,14 @@ export default function createKey(rus, eng) {
   keyRus.appendChild(shiftCaps);
   key.appendChild(keyRus);
 
-  caseDown.innerHTML = eng;
-  caseUp.innerHTML = eng.toUpperCase();
-  caps.innerHTML = eng.toUpperCase();
-  shiftCaps.innerHTML = eng;
-  keyEng.appendChild(caseDown);
-  keyEng.appendChild(caseUp);
-  keyEng.appendChild(caps);
-  keyEng.appendChild(shiftCaps);
+  caseDownEng.innerHTML = eng;
+  caseUpEng.innerHTML = eng.toUpperCase();
+  capsEng.innerHTML = eng.toUpperCase();
+  shiftCapsEng.innerHTML = eng;
+  keyEng.appendChild(caseDownEng);
+  keyEng.appendChild(caseUpEng);
+  keyEng.appendChild(capsEng);
+  keyEng.appendChild(shiftCapsEng);
   key.appendChild(keyEng);
-  console.log(key);
   return key;
 }
